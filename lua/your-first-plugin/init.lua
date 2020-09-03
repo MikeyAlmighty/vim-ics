004e9fcf7c732db5b0037740b0d7af5a6c4fbb2f
Michael <mike.leibbrandt@gmail.com>
2020-09-03 20:58:18 +0200

    Implemented Floating Window

--- lua/your-first-plugin/init.lua
+++ lua/your-first-plugin/init.lua
@@ -1,8 +1,22 @@
-local function printWindowSize()
-	-- print("Hello, World!")
-	print(vim.fn.nvim_win_get_height(0))
+local function createFloatingWindow()
+	local bufH = vim.api.nvim_create_buf(false,true)
+	-- Indexs start at 1 in Lua (FacePalm)
+	local uis = vim.api.nvim_list_uis()[1]
+
+	local width = uis.width
+	local height = uis.height
+
+	print(uis.width)
+
+	local winId = vim.api.nvim_open_win(bufH,true,{
+		relative = "editor",
+		width = width - 4,
+		height = height - 4,
+		col = 2,
+		row = 2,
+	})
 end
 
 return {
-	printWindowSize = printWindowSize
+	createFloatingWindow = createFloatingWindow
 }
